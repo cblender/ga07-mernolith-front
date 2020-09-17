00c28b9b7ac9d16c8b4051b8fe819dda29587b05
Timothy Murphy <timothymurphy@Timothys-MacBook-Pro.local>
2020-09-16 21:53:43 -0400

    added navbar component

---
+++ mernolith/src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="navbar">
+      <h1>MERNOLITH</h1>
+    </div>
+  );
+}
+
+export default Header;
